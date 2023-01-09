ebd84a32e6be879f3dedaa8c365565d4f690c355
rita <AdaRitDN@mail.ru>
2023-01-09 17:05:51 +0300

    добавлен код и блок-схема 4 задачи

---
+++ qwerty2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int a = 22;
+int b = 9;
+int c = 3;
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write ("max =");
+Console.WriteLine (max);
