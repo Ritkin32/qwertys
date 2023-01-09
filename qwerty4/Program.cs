0eec63e485ece29395d7904a8b01ebd476e717a5
rita <AdaRitDN@mail.ru>
2023-01-09 22:45:35 +0300

    задача 4 и блок-схема

---
+++ qwerty4/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int [] array = {1, 2, 3, 4, 5, 6, 7, 8};
+int N = array.Length;
+int index = 0;
+
+while (index < N)
+{
+    if(array[index] % 2 == 0)
+        {
+        Console.WriteLine (array[index]);
+        }
+    index++;
+}
+
+
+
+
+
