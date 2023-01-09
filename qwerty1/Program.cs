92a3fb29fe4b60c2d271740a7b270454adfb7459
rita <AdaRitDN@mail.ru>
2023-01-09 11:10:03 +0300

    код первой задачи

--- qwerty1/Program.cs
+++ qwerty1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+int a = -9;
+int b = -3;
+
+int max = a;
+
+if (a > max)  max = a;
+if (b> max) max = b;
+
+Console.Write ("max= ");
+Console.WriteLine (max);
