f404dd7937123b5d1ad2dea70c1209d5046151b9
rita <AdaRitDN@mail.ru>
2023-01-10 17:43:56 +0300

    лекция3

---
+++ lekzia/Program.cs
@@ -0,0 +1,28 @@
+﻿//Метод 1
+
+//void Metod1()
+
+//{
+// Console.WriteLine("Автор");
+//}
+//Metod1();
+
+// Метод2 ничего не возвращают, но могут принимать аргументы
+//void Metod2(string msg)
+//{
+//  Console.WriteLine(msg);
+//}
+//Metod2("Текст сообщения");
+
+//аргументы
+
+void Metod21(string msg, int count)
+{
+    int i = 0;
+    while (1 < count)
+    {
+      Console.WriteLine(msg);
+      count++;
+    }
+}
+Metod21(msg: "текст", count: 4);
