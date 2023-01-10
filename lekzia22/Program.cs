dc646d0af32ee246588c8fa15a1e723f497e3f2d
rita <AdaRitDN@mail.ru>
2023-01-11 00:42:05 +0300

    цикл в цикле

--- lekzia22/Program.cs
+++ lekzia22/Program.cs
@@ -37,14 +37,51 @@
 
 // цикл в цикле и for  на примере таблицы умножения
 
-for (int i = 2; i <= 10; i++)
+//for (int i = 2; i <= 10; i++)
+//{
+    //for (int j = 0; j <= 10; j++)
+   // {
+       //  Console.WriteLine($"{i}*{j} = {i * j}");
+    //}
+   //  Console.WriteLine();
+//}
+
+
+//замена букв с больших на маленькие // заменить " " на"|" "к" на "К", "С" на "с"
+
+// string s = "qwerty"
+//             012
+// s [3]= r
+
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ужели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+
+// string s = "qwerty"
+//             012345
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
 {
-    for (int j = 0; j <= 10; j++)
+      string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
     {
-         Console.WriteLine($"{i}*{j} = {i * j}");
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
     }
-     Console.WriteLine();
+    return result;
 }
 
-
-
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text, 'а', 'А');
+Console.WriteLine(newText);
+  
---
+++ qaszx/Program.cs
@@ -0,0 +1,45 @@
+﻿//найти позицию минимального элементав в неотсортированной 
+//части массива, произвести обмен этого значения со значением 
+// первой неотсортированной позиции. Повторять пока есть неотсортированные элементы.
+
+
+int[] arr = {1, 2 ,3, 4, 1, 5, 6, 7};
+
+void PrintArray (int[] array)
+{
+       int count = array.Length;
+
+       for (int i = 0; i < count; i++)
+      {
+          Console.Write($"{array[i]}");
+      }
+      Console.WriteLine();
+}
+// 2 этап метод, который будет упорядочивать наш массив
+
+void SelectionSort (int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+
+      int minPosition = i;
+
+      for (int j = i + 1; j < array.Length; j++)
+    {
+         if (array[j] < array[minPosition]) minPosition = j;
+    }
+       int temporary = array[i];
+       array[i] = array[minPosition];
+      array[minPosition] = temporary;  
+
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
+
+
+
+
