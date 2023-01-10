1cbaa3e1996e7b8213a93e3ff00ddae31e51a083
rita <AdaRitDN@mail.ru>
2023-01-10 19:14:18 +0300

    4 метод

---
+++ lekzia22/Program.cs
@@ -0,0 +1,19 @@
+﻿// 4 метод
+
+string Metod4(int count, string text) // правой кнопкой мыши ренейм буква изм на текст
+{
+    int i = 0;
+    string result = string.Empty; // строка пусто значение
+
+      while (i < count)
+      {
+          result = result + text;
+          i++;
+      }
+
+    return result;
+}
+
+string res = Metod4(10, "z");
+Console.WriteLine(res);
+
